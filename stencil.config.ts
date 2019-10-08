d0f33a4d1895b992ddb3dc6d78641efb34e683df
Haldun Yildiz <haldun313@gmail.com>
2019-10-09 00:00:42 +0300

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'pwc-map-legend',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
